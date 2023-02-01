924d16ac5c43d3e89938ce2d6031695d6ca1423a
Aelita Jelden <nyc.aelita@gmail.com>
2023-02-01 14:09:31 +0600

    Practice with Chirag materials

---
+++ src/com/java/practice/Chirag/SwapValue6.java
@@ -0,0 +1,18 @@
+package com.java.practice.Chirag;
+
+public class SwapValue6 {
+    public static void main(String[] args) {
+        // to swap the value without using third variable
+
+        int x = 10;
+        int y = 36;
+
+        x = x + y; // 10 + 36 = 46
+        y = x - y; // 46 - 36 = 10;
+        x = x - y; // 46 - 10 = 36;
+
+        System.out.println("Value of X is " + x);
+        System.out.println("Value of Y is " + y);
+
+    }
+}
