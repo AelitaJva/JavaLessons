de5c87497d0869f2ccf0f7ea83ed7d5b73297cb6
Aelita Jelden <nyc.aelita@gmail.com>
2023-02-05 23:56:00 +0600

    nested for loop practice

---
+++ src/com/java/homeWork/hw21/task04.java
@@ -0,0 +1,20 @@
+package com.java.homeWork.hw21;
+
+public class task04 {
+    /*
+     *
+     **
+     ***
+     ****
+     *****
+     * */
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
