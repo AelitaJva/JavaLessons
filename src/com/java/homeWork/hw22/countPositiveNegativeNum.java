74a3ae6b880d23634af11d092603bf86ebd9fab7
Aelita Jelden <nyc.aelita@gmail.com>
2023-02-06 17:59:43 +0600

    practice arrays

---
+++ src/com/java/homeWork/hw22/countPositiveNegativeNum.java
@@ -0,0 +1,27 @@
+package com.java.homeWork.hw22;
+
+import java.util.Scanner;
+
+public class countPositiveNegativeNum {
+    public static void main(String[] args) {
+        int listOfNumbers [] = new int[7];
+        int positiveNum = 0;
+        int negativeNum = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 7 numbers: ");
+        for (int i=0; i<listOfNumbers.length; i++) {
+            listOfNumbers [i] = sc.nextInt();
+        }
+
+        for (int i=0; i<listOfNumbers.length; i++) {
+            if(listOfNumbers[i] >= 0) {
+                positiveNum++;
+            } else {
+                negativeNum++;
+            }
+        }
+        System.out.println("Amount of positive numbers is: " + positiveNum);
+        System.out.println("Amount of negative numbers is: " + negativeNum);
+    }
+}
---
+++ src/com/java/homeWork/hw22/sumOfEvenAndOddNumber.java
@@ -0,0 +1,35 @@
+package com.java.homeWork.hw22;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class sumOfEvenAndOddNumber {
+    public static void main(String[] args) {
+        int listOfNumbers[] = new int[7];
+        int sumOfEven = 0;
+        int sumOfOdd =  0;
+
+        Scanner sc = new Scanner(System.in);
+        out.println("Enter 7 numbers");
+
+        for (int i=0; i<listOfNumbers.length; i++) {
+            listOfNumbers[i] = sc.nextInt();
+        }
+
+        for (int i=0; i<listOfNumbers.length; i++) {
+            if(listOfNumbers[i] % 2 == 0) {
+                sumOfEven+=listOfNumbers[i];
+            } else {
+                sumOfOdd+=listOfNumbers[i];
+            }
+        }
+
+        out.println("Sum of even numbers is: " + sumOfEven);
+        out.println("Sum of odd numbers is: " + sumOfOdd);
+
+
+    }
+}
+
+
