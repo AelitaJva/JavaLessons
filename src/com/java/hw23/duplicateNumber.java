23cb5d9c423ddf79d0acf3471cad8b61f78bc88e
Aelita Jelden <nyc.aelita@gmail.com>
2023-02-08 21:52:23 +0600

    arrays with example

---
+++ src/com/java/hw23/duplicateNumber.java
@@ -0,0 +1,25 @@
+package com.java.hw23;
+
+import java.util.Scanner;
+
+public class duplicateNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 8 numbers: ");
+
+        int num [] = new int[8];
+        for (int i=0; i<8; i++) {
+            num [i] = sc.nextInt();
+        }
+    }
+
+    public static void duplicateNumber (int [] num) {
+        for (int i=0; i<num.length; i++) {
+            for (int j=i+1; j<num.length; j++) {
+                if(num[i] == num[j]) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
---
+++ src/com/java/hw23/reverseNum.java
@@ -0,0 +1,32 @@
+package com.java.hw23;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class reverseNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter numbers from 1 to 5: ");
+        int collectionOfNumbers [] = new int[5];
+
+        for (int i=0; i<collectionOfNumbers.length; i++) {
+            collectionOfNumbers [i] = sc.nextInt();
+        }
+
+        System.out.println("Entered numbers: " + Arrays.toString(collectionOfNumbers));
+        System.out.print("Reversed numbers: ");
+
+        reverseNumbers(collectionOfNumbers);
+    }
+
+
+    public static void reverseNumbers (int [] collectionOfNumbers) {
+       for (int i = collectionOfNumbers.length-1; i>=0; i--) {
+           System.out.print(collectionOfNumbers[i] + " ");
+       }
+
+       // System.out.println();
+       // System.out.println();
+    }
+
+}
