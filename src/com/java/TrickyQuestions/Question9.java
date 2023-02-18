8576b6cc8c790a8223a896fb1d2de6d03ec05633
Aelita Jelden <nyc.aelita@gmail.com>
2023-02-18 22:58:30 +0600

    Tricky OOP question

---
+++ src/com/java/TrickyQuestions/Question9.java
@@ -0,0 +1,26 @@
+package com.java.TrickyQuestions;
+
+class Test {
+    int count;
+
+    Test () {
+        count++;
+    }
+
+    public void printNumber () {
+        System.out.println(count);
+    }
+}
+public class Question9 {
+    public static void main(String[] args) {
+        Test d1 = new Test ();
+        Test d2 = new Test ();
+        Test d3 = new Test ();
+        Test d4 = new Test ();
+
+        d1.printNumber();
+        d2.printNumber();
+        d3.printNumber();
+        d4.printNumber();
+    }
+}
